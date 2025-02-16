ce172c61e8d31322612964c77e349e1f9f9050fa
MD ABDUR RAHAMAN <abdurrahaman444.spy.com>
2025-02-16 08:46:27 +0600

    make singup logic

--- app/Http/Controllers/API/AutController.php
+++ app/Http/Controllers/API/AutController.php
@@ -3,9 +3,43 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AutController extends Controller
 {
-    //
+    public function signup(Request $request){
+        $validateUser= Validator::make(
+            $request->all(),
+            [
+                     'name'=>'required',
+                     'email'=> 'required|email|unique:users,email',
+                     'password'=>'required',
+                    ]
+            );
+            if($validateUser->fails()){
+                return response()->json([
+                    'status'=>false,
+                    'message'=>'Validation Error',
+                    'error'=> $validateUser->errors()->all()
+                ],401);
+            }
+            $user = User::create([
+                'name'=>$request->name,
+                'email'=>$request->email,
+                'password'=>$request->password,
+            ]);
+            return response()->json([
+                'status'=>true,
+                'message'=>'User Created Successfully',
+                'user'=>$user,
+            ],200 );
+    }
+    public function login(Request $request){
+
+    }
+    public function logout(Request $request){
+
+    }
 }
