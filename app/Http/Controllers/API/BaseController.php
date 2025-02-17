a244f2570c951bd626204daf25713d93b797daec
MD ABDUR RAHAMAN <abdurrahaman444.spy.com>
2025-02-17 12:16:36 +0600

    basecontroller make

---
+++ app/Http/Controllers/API/BaseController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BaseController extends Controller
+{
+    public function sendResponse($result,$message){
+        $response = [
+            'success' => true,
+            'data' => $result,
+            'message' => $message,
+        ];
+        return response()->json($response,200);
+    }
+    public function sendError($error,$errormessage =[],$code = 404){
+        $response = [
+            'success' => false,
+            'message' => $error,
+        ];
+        if(!empty($errormessage)){
+            $response['data'] = $errormessage;
+        }
+        return response()->json($response,$code);
+    }
+}
